c1f4c663f37209a2fb9aa71fa9e87a4f122ab523
keck <akeck@usna94.com>
2023-05-04 12:34:43 -0400

    Initial Commit

---
+++ GymBuds/GymBudsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymBudsApp.swift
+//  GymBuds
+//
+//  Created by Andrew Keck on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct GymBudsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
